805563107c73c0b0e19b55de457dbdd3bb0d7e69
jacobschenberg <66764618+jacobschenberg@users.noreply.github.com>
2020-06-12 13:40:40 -0700

    Update test_functions.py

--- cogs18.project/my_module/test_functions.py
+++ cogs18.project/my_module/test_functions.py
@@ -5,21 +5,21 @@ from functions import begin_character, cyan_saber, emitter, finish
 
 
 def test_begin_character():
-
 	assert callable(begin_character)
 	assert begin_character is not None
 
+	
 def test_cyan_saber():
 	assert callable(cyan_saber)
 	assert cyan_saber is not None
 
+	
 def test_emitter():
 	assert callable(emitter)
 	assert emitter is not None
 
 
 def test_finish():
-
     	assert callable(finish)
 	assert finish is not None
 	assert finish('Darth', 'Jacob', 'curved-handled', 'blue', 'magus', 'BB') ==
@@ -28,9 +28,3 @@ def test_finish():
 
 
 #!pytest test_functions/
-    
-
-
-
-                 
-    
